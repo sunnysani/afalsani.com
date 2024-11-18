25df38a97c5ab06fd78ec6527b9384e9634f18c7
Naufal Sani <naufal.sani2001@gmail.com>
2024-11-18 13:49:41 +0700

    Move waiting svg to statically at page

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -11,12 +11,7 @@ const LoadingPage = () => {
       }}
     >
       <div>
-        <img
-          src="/public/icons/bouncing-circles.svg"
-          width={150}
-          height={150}
-          alt="loading"
-        />
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200' width={150} height={150}><circle fill='#7E99D8' stroke='#7E99D8' stroke-width='15' r='15' cx='40' cy='65'><animate attributeName='cy' calcMode='spline' dur='2' values='65;135;65;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='-.4'></animate></circle><circle fill='#7E99D8' stroke='#7E99D8' stroke-width='15' r='15' cx='100' cy='65'><animate attributeName='cy' calcMode='spline' dur='2' values='65;135;65;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='-.2'></animate></circle><circle fill='#7E99D8' stroke='#7E99D8' stroke-width='15' r='15' cx='160' cy='65'><animate attributeName='cy' calcMode='spline' dur='2' values='65;135;65;' keySplines='.5 0 .5 1;.5 0 .5 1' repeatCount='indefinite' begin='0'></animate></circle></svg>
       </div>
       <h2>Loading Page</h2>
     </div>
