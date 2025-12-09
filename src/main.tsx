569c6e855248ec64e2d085c820a5038f42f21e73
Naufal Sani <naufal.sani2001@gmail.com>
2025-12-09 16:56:16 +0700

    Change MUI to tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "@mui/material";
-import theme from "./theme";
 import ErrorBoundary from "./utils/routing/ErrorBoundary";
 import CrashPage from "./pages/CrashPage";
 import AppRouter from "./utils/routing/AppRouter";
@@ -9,10 +7,8 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <ErrorBoundary fallback={CrashPage}>
-        <AppRouter />
-      </ErrorBoundary>
-    </ThemeProvider>
+    <ErrorBoundary fallback={CrashPage}>
+      <AppRouter />
+    </ErrorBoundary>
   </StrictMode>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,5 @@
-import ExperienceSection from "@/components/page-components/index-page/ExperienceSection";
-import GreetingSection from "@/components/page-components/index-page/GreetingsSection";
-import TechnologyStack from "@/components/page-components/index-page/TechnologyStackSection";
-import { Button, Stack, Typography, useMediaQuery } from "@mui/material";
-import { Link } from "react-router-dom";
-
 const MainPage = () => {
-  const tooSmall = useMediaQuery(`(max-width:${752}px)`);
-
-  return (
-    <Stack direction="column" position="relative">
-      <Link
-        to="https://www.linkedin.com/in/naufal-sani/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Button
-          sx={{ position: "absolute", top: 60, right: tooSmall ? 20 : 60 }}
-        >
-          Say Hi!
-        </Button>
-      </Link>
-      <GreetingSection />
-      <ExperienceSection />
-      <TechnologyStack />
-      <Typography
-        sx={{ mt: 8, mb: 8 }}
-        textAlign="center"
-        variant="Title3Regular"
-        fontStyle="italic"
-      >
-        Another component is still under development
-      </Typography>
-    </Stack>
-  );
+  return <div>Light Rays</div>;
 };
 
 export default MainPage;
